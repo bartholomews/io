98c30e94d206498c5b4f9abf0a3cefa306b8fb71
bartholomews <github@bartholomews.io>
2023-03-18 16:32:33 +0000

    get rid of vue

--- src/components/bootstrap.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-import { BContainer, BCol, BRow } from 'bootstrap-vue';
-
-Vue.component('b-container', BContainer);
-Vue.component('b-col', BCol);
-Vue.component('b-row', BRow);
